4a923240b56106f237cabf6fbb0335b3656b65c0
Mordecai Isheanesu Musinahama <55081726+Mordecaimusinahama@users.noreply.github.com>
2025-07-12 13:04:56 +0200

    Delete farmers-connect-app directory

--- farmers-connect-app/src/hooks/useSample.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { SampleContext } from '../context/SampleContext';
-
-export function useSample() {
-  return useContext(SampleContext);
-} 
--- farmers-connect-app/src/utils/sampleUtil.js
+++
@@ -1,3 +0,0 @@
-export function greetUser(name) {
-  return `Hello, ${name}! Welcome to Farmers Connector.`;
-} 
